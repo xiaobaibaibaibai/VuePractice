83b95b2306e0344e45f866f37996fdd766473a67
Wentan <wentan1995@gmail.com>
2020-06-15 05:04:03 -0400

    encapsulation axios

--- demo-js/src/views/TaskManagement.vue
+++ demo-js/src/views/TaskManagement.vue
@@ -9,6 +9,37 @@
     <br><br>
     <button @click="test">print</button>
     <br><br>
+    <el-button type="primary" @click="test">按钮</el-button>
+    <br><br>
+
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="活动名称">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+
+      <el-form-item label="活动名称">
+        <el-upload
+          class="upload-demo"
+          action="http://localhost:5000/test/test1/"
+          :on-preview="handlePreview"
+          :on-remove="handleRemove"
+          :before-remove="beforeRemove"
+          multiple
+          :limit="3"
+          :on-exceed="handleExceed"
+          :file-list="fileList"
+          >
+          <el-button size="small" type="primary">点击上传</el-button>
+          <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+        </el-upload>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">立即创建</el-button>
+        <el-button>取消</el-button>
+      </el-form-item>
+    </el-form>
+
     <ShowTasks />
   </div>
 </template>
@@ -17,11 +48,20 @@
 // @ is an alias to /src
 import ShowTasks from '@/components/ShowTasks.vue'
 import { mapActions } from 'vuex';
+import request from './request'
 
 export default {
   name: 'TaskManagement',
   components: {
     ShowTasks
+  },
+  data() {
+    return {
+      form: {
+        name: '',
+      },
+      fileList:[],
+    }
   },
   methods: {
     ...mapActions([
@@ -34,8 +74,34 @@ export default {
       }
       e.target.value = '';
     },
+    onSubmit() {
+      console.log('submit!');
+    },
     test() {
-      console.log(this.$store.state.tasks);
+      request({
+        url: '/test/test1',
+        method: 'post',
+        data: {
+          form: this.form
+        }
+      }).then(response => {
+        console.log('in vue, response');
+        console.log(response);
+      })
+    },
+
+
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePreview(file) {
+      console.log(file);
+    },
+    handleExceed(files, fileList) {
+      this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
+    },
+    beforeRemove(file) {
+      return this.$confirm(`确定移除 ${ file.name }？`);
     }
   },
 }
---
+++ demo-js/src/views/request.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+// create an axios instance
+const service = axios.create({
+  // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  baseURL: 'http://localhost:5000/',
+  // withCredentials: true, // send cookies when cross-domain requests
+  timeout: 5000 // request timeout
+})
+
+// request interceptor
+service.interceptors.request.use(
+  config => {
+    console.log('In request.js');
+    console.log('this request: ' + config.url)
+    // do something before request is sent
+    return config
+  },
+  error => {
+    // do something with request error
+    console.log(error) // for debug
+    return Promise.reject(error)
+  }
+)
+
+export default service
